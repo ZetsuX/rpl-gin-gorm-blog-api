522685f482e9eb0e6ebbe7a4d5dad646fb124d95
Fehlling Halim <kevnhalim@gmail.com>
2023-03-11 17:25:11 +0700

    fix : removing password hash for unchanged password

--- entity/user.go
+++ entity/user.go
@@ -28,7 +28,7 @@ func (u *User) BeforeCreate(tx *gorm.DB) error {
 }
 
 func (u *User) BeforeUpdate(tx *gorm.DB) error {
-	if u.Password != "" {
+	if u.Password != "" && tx.Statement.Changed("Password") {
 		var err error
 		u.Password, err = utils.PasswordHash(u.Password)
 		if err != nil {
